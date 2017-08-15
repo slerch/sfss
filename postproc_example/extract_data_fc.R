86edf55c2911ffb3011ff681693909b9853e601b
SL <sl-research@outlook.com>
2017-08-15 12:23:26 +0200

    add 2016 fc data

--- postproc_example/extract_data_fc.R
+++ postproc_example/extract_data_fc.R
@@ -110,3 +110,57 @@ save(ensfc2015, dates2015, file = paste0(data_dir, "ensfc2015.Rdata"))
 nc_close(data_nc)
 
 ## repeat for 2016
+rm(list=ls())
+
+library(ncdf4)
+data_dir <- "/home/sebastian/Dropbox/ERC SummerSchool/software_tutorials/postproc_example/data/"
+data_nc <- nc_open(paste0(data_dir,"ensfc2016.nc"))
+
+# extract variables with ncvar_get
+uwind <- ncvar_get(nc = data_nc, varid = "u10")
+vwind <- ncvar_get(nc = data_nc, varid = "v10")
+
+# identify content and dimension interpretation for uwind, vwind
+dim(uwind)
+dim_meanings <- c(data_nc$var$u10$dim[[1]]$name,
+                  data_nc$var$u10$dim[[2]]$name,
+                  data_nc$var$u10$dim[[3]]$name,
+                  data_nc$var$u10$dim[[4]]$name)
+dim_meanings
+
+time <- ncvar_get(nc = data_nc, varid = "time")
+# strange time format:
+ncatt_get(nc = data_nc, varid = "time") # "hours since 1900-01-01 00:00:0.0"
+# convert to standard format matching observations
+time_converted <- as.POSIXlt(time*3600, tz = "UTC", origin = "1900-01-01 00:00")
+dates <- as.Date(time_converted)
+
+lats <- ncvar_get(nc = data_nc, varid = "latitude")
+lons <- ncvar_get(nc = data_nc, varid = "longitude")
+
+# position indicators for "Heidelberg"
+posHD_lat <- which(lats == 49)
+posHD_lon <- which(lons == 8)
+
+# extract relevant entries of u and v wind components 
+uwind_fc <- t(uwind[posHD_lon, posHD_lat,,])
+vwind_fc <- t(vwind[posHD_lon, posHD_lat,,])
+
+ensfc2016 <- sqrt(uwind_fc^2 + vwind_fc^2)
+dates2016 <- dates
+
+save(ensfc2016, dates2016, file = paste0(data_dir, "ensfc2016.Rdata"))
+
+nc_close(data_nc)
+
+## combine
+rm(list=ls())
+
+data_dir <- "/home/sebastian/Dropbox/ERC SummerSchool/software_tutorials/postproc_example/data/"
+load(paste0(data_dir, "ensfc2015.Rdata"))
+load(paste0(data_dir, "ensfc2016.Rdata"))
+
+fcdates <- c(dates2015, dates2016)
+ensfc <- rbind(ensfc2015, ensfc2016)
+
+save(fcdates, ensfc, file = paste0(data_dir, "HDwind_ensfc.Rdata"))
